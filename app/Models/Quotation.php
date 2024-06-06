9de6a815609668ed5ca90476c2ec83de1cddc56a
ArthurEstevaum <arthurestevao631@gmail.com>
2024-06-06 14:37:13 -0300

    Dispatch quotation created event from model Quotation

--- app/Models/Quotation.php
+++ app/Models/Quotation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\QuotationCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,6 +16,10 @@ class Quotation extends Model
         'name',
         'shipping_address',
         'notes',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => QuotationCreated::class,
     ];
 
     public function budgets(): HasMany
